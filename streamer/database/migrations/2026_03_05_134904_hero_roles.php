078ef7e045fece4aa815344c65d732939b83b100
NoelZeyn <127940099+NoelZeyn@users.noreply.github.com>
2026-03-05 20:55:52 +0700

    test migration v1

---
+++ streamer/database/migrations/2026_03_05_134904_hero_roles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hero_roles', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('hero_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('role_id')
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ streamer/database/migrations/2026_03_05_135128_match_players.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('match_players', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('match_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('player_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('hero_id')
+                ->constrained();
+
+            $table->foreignId('role_id')
+                ->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
