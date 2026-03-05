0cb5ef39f5e0701de5b6858f303158d712a9730f
NoelZeyn <127940099+NoelZeyn@users.noreply.github.com>
2026-03-06 00:41:08 +0700

    Controller test abstract & API

--- streamer/app/Http/Controllers/Controller.php
+++ streamer/app/Http/Controllers/Controller.php
@@ -4,5 +4,20 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function success($data, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function error($message, $code = 400)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message
+        ], $code);
+    }
 }
--- streamer/routes/api.php
+++ streamer/routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MatchController;
 use App\Http\Controllers\PlayerController;
+use Illuminate\Container\Attributes\Auth;
 
 Route::prefix('v1')->middleware('auth:api')->group(function () {
     Route::post('/matches', [MatchController::class, 'store']);
     Route::get('/players', [PlayerController::class, 'index']);
     Route::post('/players', [PlayerController::class, 'store']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
